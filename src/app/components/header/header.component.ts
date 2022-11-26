7adfabca5e4c1ce51d152587228edf348a721786
Jose DeLavalle <me@josedelavalle.com>
2022-11-25 20:58:22 -0500

    responsive header

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -69,6 +69,7 @@ export class HeaderComponent implements OnInit, OnDestroy {
       verticalPosition: 'bottom',
       duration: 12000
     });
+    window.dispatchEvent(new Event('resize'));
   }
   
   goToHeatmap() {
